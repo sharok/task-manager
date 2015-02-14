6cb617740a922a92e8416b3b0318077f7dcd2269
Max Drachsh <sharok89@gmail.com>
2015-02-15 04:50:00 +0800

    Реализован модуль для проверки аутентифицирован ли пользователь.

--- server/modules/security.js
+++ server/modules/security.js
@@ -1,9 +1,24 @@
-module.exports = {
-    checkAuthenticated: function (req, res, next) {
+'use strict'
+
+var security = {
+    apiUserLoggedIn: function (req, res, next) {
         if (req.isAuthenticated()) {
             return next();
         }
 
-        res.status(401).send({error: 'not authenticated'});
+        res.status(401).send({error: 'User is not authenticated'})
+
+    },
+
+    siteUserLoggedIn: function (req, res, next) {
+        if (req.isAuthenticated()) {
+            return next();
+        }
+
+        res.redirect('/');
+
     }
-};
+
+}
+
+module.exports = security;
