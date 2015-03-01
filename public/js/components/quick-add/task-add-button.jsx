596fc92961f885705a24236cd9d1a2c791b45458
atomiomi <atomio.ak@gmail.com>
2015-03-01 20:30:20 +0600

    fixed gitignore and task-add-button

--- public/js/components/quick-add/task-add-button.jsx
+++ public/js/components/quick-add/task-add-button.jsx
@@ -1,6 +1,7 @@
 "use strict"
 
 var React = require('react'),
+    lz = require('localization').get(),
     quickTaskAddActions = require('actions/quickTaskAddActions'),
     SvgIco = require('../svg-ico.jsx');
 
